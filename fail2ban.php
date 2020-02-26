5e4941aab90229166e1f55a5b04caeeaa7cb7118
mkrumbholz <31690023+mkrumbholz@users.noreply.github.com>
2020-02-26 10:57:57 +0100

    Update fail2ban.php

--- fail2ban.php
+++ fail2ban.php
@@ -2,7 +2,7 @@
 /**
  * RoundCube Fail2Ban Plugin
  *
- * @version 1.1
+ * @version 1.3
  * @author Matt Rude [m@mattrude.com]
  * @url http://mattrude.com/plugins/roundcube-fail2ban-plugin/
  * @license GPLv3
@@ -16,6 +16,7 @@ class fail2ban extends rcube_plugin
 
   function log($args)
   {
-    rcmail::write_log('userlogins', 'FAILED login for ' .$args['user']. ' from ' .getenv('REMOTE_ADDR'));
+    $remote_addr = !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ? $_SERVER['HTTP_X_FORWARDED_FOR'] : $_SERVER['REMOTE_ADDR'];
+    rcmail::write_log('userlogins', 'FAILED login for ' .$args['user']. ' from ' .$remote_addr);
   }
 }
